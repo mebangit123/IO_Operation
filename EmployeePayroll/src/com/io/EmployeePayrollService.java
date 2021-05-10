cc271b8aa73ffb7631386da3d5b1d5ece9c70025
mebangit123 <mebannongrum333@gmail.com>
2021-05-10 12:35:59 +0530

    Creating employee payroll service

--- EmployeePayroll/src/com/io/EmployeePayrollService.java
+++ EmployeePayroll/src/com/io/EmployeePayrollService.java
@@ -1,7 +1,60 @@
 package com.io;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class EmployeePayrollService {
+	private List<EmployeePayrollData> empPayrollList;
+
+	public EmployeePayrollService() {
+	}
+
+	public EmployeePayrollService(List<EmployeePayrollData> data) {
+		this.empPayrollList = data;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Java IO - Employee Payroll Service");
+		
+		List<EmployeePayrollData> datas = new ArrayList<EmployeePayrollData>();
+		EmployeePayrollService service = new EmployeePayrollService(datas);
+		service.readData(new Scanner(System.in));
+
+		service.writeData();
+	}
+
+	public void readData(Scanner sc) {
+		System.out.println("Enter ID");
+		int id = sc.nextInt();
+
+		System.out.println("Enter Salary");
+		int sal = sc.nextInt();
+
+		System.out.println("Enter Name");
+		String name = sc.next();
+
+		empPayrollList.add(new EmployeePayrollData(id, sal, name));
+	}
+
+	public void writeData() {
+		System.out.println(empPayrollList);
+	}
+}
+
+class EmployeePayrollData {
+	private int id;
+	private int salary;
+	private String name;
+
+	public EmployeePayrollData(int id, int salary, String name) {
+		this.id = id;
+		this.salary = salary;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeePayrollData [id=" + id + ", salary=" + salary + ", name=" + name + "]";
 	}
 }
